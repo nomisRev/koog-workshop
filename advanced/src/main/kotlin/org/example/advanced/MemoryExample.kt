b6c2b7cec68c718294caf653b337e68841371191
Andrey Bragin <andrey.bragin@jetbrains.com>
2026-05-20 16:01:36 +0200

    update memory example

--- advanced/src/main/kotlin/org/example/advanced/MemoryExample.kt
+++ advanced/src/main/kotlin/org/example/advanced/MemoryExample.kt
@@ -1,14 +1,17 @@
 package org.example.advanced
 
+import ai.koog.agents.chatMemory.feature.ChatMemory
 import ai.koog.agents.core.agent.AIAgent
 import ai.koog.agents.core.agent.config.AIAgentConfig
 import ai.koog.agents.core.tools.ToolRegistry
 import ai.koog.agents.core.tools.annotations.LLMDescription
 import ai.koog.agents.core.tools.annotations.Tool
+import ai.koog.agents.core.tools.reflect.ToolSet
 import ai.koog.agents.core.tools.reflect.asTool
 import ai.koog.agents.features.eventHandler.feature.EventHandler
 import ai.koog.agents.longtermmemory.feature.LongTermMemory
 import ai.koog.agents.longtermmemory.ingestion.IngestionSettings
+import ai.koog.agents.longtermmemory.model.MemoryRecord
 import ai.koog.agents.longtermmemory.retrieval.RetrievalSettings
 import ai.koog.agents.longtermmemory.retrieval.search.SimilaritySearchStrategy
 import ai.koog.embeddings.local.LLMEmbedder
@@ -17,6 +20,7 @@ import ai.koog.prompt.executor.clients.openai.OpenAILLMClient
 import ai.koog.prompt.executor.clients.openai.OpenAIModels
 import ai.koog.prompt.executor.llms.MultiLLMPromptExecutor
 import ai.koog.prompt.llm.LLMProvider
+import ai.koog.rag.base.storage.search.SimilaritySearchRequest
 import ai.koog.rag.vector.storage.JVMFileEmbeddingStorage
 import ai.koog.spring.ai.vectorstore.SpringAiKoogVectorStore
 import kotlinx.coroutines.Dispatchers
@@ -40,7 +44,8 @@ suspend fun main() {
     MultiLLMPromptExecutor(
         LLMProvider.OpenAI to OpenAILLMClient(openaiApiKey)
     ).use { promptExecutor ->
-        runSimpleAgent(promptExecutor, openaiApiKey)
+        // runSimpleAgent(promptExecutor, openaiApiKey)
+        runMemoryToolsAgent(promptExecutor, openaiApiKey)
     }
 }
 
@@ -106,8 +111,6 @@ private suspend fun runSimpleAgent(
             prompt = prompt("example") {
                 system {
                     +"You're a helpful assistant."
-                    +"Use sendMessage tool to chat with the user, DO NOT send regular messages to chat."
-                    +"Only send the normal message to end the conversation when the user is done."
                 }
             },
             model = OpenAIModels.Chat.GPT5_5,
@@ -148,18 +151,99 @@ private suspend fun runSimpleAgent(
                 val prompt = ctx.prompt
             }
         }
+
+        install(ChatMemory)
     }
 
-    val initialMessage = sendMessage("Hi, how can I help you?")
-    val result = agent.run(initialMessage)
-    println("Agent: $result")
+    var userMessage = sendMessage("Hi, how can I help you?")
+
+    while (userMessage != "/q") {
+        val result = agent.run(userMessage)
+        userMessage = sendMessage(result)
+    }
+}
+
+private suspend fun runMemoryToolsAgent(
+    promptExecutor: MultiLLMPromptExecutor,
+    openaiApiKey: String,
+) {
+    val vectorStore = provideSpringPgVectorStore(openaiApiKey)
+
+    val sendMessageTool = ::sendMessage.asTool()
+    val memoryTools = MemoryTools(vectorStore)
+
+    val agent = AIAgent(
+        promptExecutor = promptExecutor,
+        agentConfig = AIAgentConfig(
+            prompt = prompt("example") {
+                system {
+                    +"You're a helpful assistant."
+                }
+            },
+            model = OpenAIModels.Chat.GPT5_5,
+            maxAgentIterations = 100,
+        ),
+        toolRegistry = ToolRegistry {
+            tool(sendMessageTool)
+            tools(memoryTools)
+        }
+    ) {
+        install(EventHandler) {
+            onLLMCallStarting { ctx ->
+                val prompt = ctx.prompt
+            }
+
+            onLLMCallCompleted { ctx ->
+                val prompt = ctx.prompt
+            }
+        }
+
+        install(ChatMemory)
+    }
+
+    var userMessage = sendMessage("Hi, how can I help you?")
+
+    while (userMessage != "/q") {
+        val result = agent.run(userMessage)
+        userMessage = sendMessage(result)
+    }
 }
 
 @Tool
-@LLMDescription("Shows a message to the user and returns the user response. Call this tool to chat with the user.")
-fun sendMessage(message: String): String {
+@LLMDescription("Shows a message to the user and returns the user response. ALWAYS use this tool to ask follow-up question to the user.")
+private fun sendMessage(
+    @LLMDescription("The message to show to the user")
+    message: String
+): String {
     println("Agent: $message")
     print("User: ")
     return readln()
 }
 
+
+class MemoryTools(
+    private val vectorStore: SpringAiKoogVectorStore,
+) : ToolSet {
+    @Tool
+    @LLMDescription("Stores facts in the long term memory. Returns true if successful, false otherwise.")
+    suspend fun storeMemory(facts: List<String>): Boolean {
+        val memories = vectorStore.add(
+            facts.map { MemoryRecord(it) }
+        )
+
+        return memories.isNotEmpty()
+    }
+
+    @Tool
+    @LLMDescription("Searches the long term memory for relevant facts. Return empty list if no relevant facts were found")
+    suspend fun searchMemory(query: String): List<String> {
+        return vectorStore
+            .search(
+                SimilaritySearchRequest(
+                    queryText = query,
+                    minScore = 0.2,
+                )
+            )
+            .map { it.document.content }
+    }
+}
